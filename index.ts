8903b40d5d2cc6906b5236f2fe107c843f4bcaaa
RuscalWorld <me@ruscalworld.ru>
2022-11-24 17:27:08 +0300

    Использовать новый API для получения статистики

--- index.ts
+++ index.ts
@@ -1,29 +1,40 @@
 import Leaderboard from './src/Leaderboard.js'
 import BortexelAPI from './src/BortexelAPI.js'
 import ActivityWatcher from './src/ActivityWatcher.js'
+import fs from 'fs'
 
 async function start() {
-    let token = process.env.BORTEXEL_TOKEN
-    let url = process.env.BORTEXEL_URL
+    const token = process.env.BORTEXEL_TOKEN
+    const url = process.env.BORTEXEL_URL
     if (typeof token === 'undefined' || typeof url === 'undefined') {
         console.error('BORTEXEL_TOKEN or BORTEXEL_URL is not set')
         return
     }
 
-    let leaderboard = new Leaderboard('https://api.bortexel.net/method/leaderboard/get.php?extended=true')
-    let playersActivity = await leaderboard.fetchPlayersActivity()
+    const limitExpansionKey = process.env.STATS_LIMIT_EXPANSION_KEY
+    if (typeof limitExpansionKey === 'undefined') {
+        console.error('STATS_LIMIT_EXPANSION_KEY is not set')
+        return
+    }
 
-    let watcher = new ActivityWatcher(playersActivity)
+    const servers = JSON.parse(fs.readFileSync('servers.json', 'utf-8'))
+
+    const leaderboard = new Leaderboard('https://stats.bortexel.net/', limitExpansionKey)
+    const playersActivity = await leaderboard.fetchTotalActivity(servers)
+
+    if (!playersActivity) return
+    const watcher = new ActivityWatcher(playersActivity)
     await watcher.load()
-    let recentlyPlayed = watcher.getRecentlyPlayed()
+    const recentlyPlayed = watcher.getRecentlyPlayed()
     await watcher.save()
 
-    let api = new BortexelAPI({
+    const api = new BortexelAPI({
         token, url,
     }, {
         requiredHours: 3,
         activeDays: 3,
     })
+
     await api.fetchAllUsers()
     await api.reportInactive()
     await api.reportActive(recentlyPlayed)
--- src/Leaderboard.ts
+++ src/Leaderboard.ts
@@ -1,24 +1,69 @@
-import unfetch from 'isomorphic-unfetch'
+import fetch from 'node-fetch'
+
+const playTimeField = {
+    groupName: 'bortexel:totals',
+    fieldName: 'bortexel:play_time'
+}
 
 export default class Leaderboard {
     private readonly url: string
+    private readonly key: string
 
-    constructor(url: string) {
+    constructor(url: string, key: string) {
         this.url = url
+        this.key = key
     }
 
-    async fetchPlayersActivity() {
-        console.log('Fetching activity of players from leaderboard')
+    async fetchTotalActivity(servers: {
+        season: number,
+        serverName: string,
+    }[]): Promise<{ [key: string]: number } | null> {
+        const totalActivity : { [key: string]: number } = {}
+
+        for (const server of servers) {
+            const serverActivity = await this.fetchServerActivity(server)
+            if (serverActivity == null) {
+                console.error(`Skipping handling of activity of ${ server.serverName } #${ server.season }`)
+                continue
+            }
+
+            for (const playerID of Object.keys(serverActivity)) {
+                if (totalActivity.hasOwnProperty(playerID)) {
+                    totalActivity[playerID] += serverActivity[playerID]
+                } else totalActivity[playerID] = serverActivity[playerID]
+            }
+        }
+
+        return totalActivity
+    }
+
+    async fetchServerActivity(server: {
+        season: number,
+        serverName: string,
+    }): Promise<{ [key: string]: number } | null> {
+        console.log(`Fetching activity for server ${ server.serverName } #${ server.season } from stats server`)
         let start = new Date().getTime()
 
-        let response = await unfetch(this.url)
+        let response = await fetch(this.url, {
+            method: 'POST',
+            body: JSON.stringify({
+                server,
+                filter: [ playTimeField ],
+                sort: {
+                    field: playTimeField,
+                    direction: 'descending',
+                },
+                limitExpansionKey: this.key,
+            })
+        })
+
         let data = await response.json()
         let players: { [key: string]: number } = {}
 
-        for (let i in data.response) {
-            if (!data.response.hasOwnProperty(i)) continue
-            let entry = data.response[i]
-            players[entry.player] = entry.total_played
+        // @ts-ignore
+        for (const player of data) {
+            // TODO: consider using UUIDs
+            players[player.name] = player.stats['bortexel:totals']['bortexel:play_time']
         }
 
         console.log(`Successfully fetched activity for ${ Object.keys(players).length } players in ${ new Date().getTime() - start }ms`)
